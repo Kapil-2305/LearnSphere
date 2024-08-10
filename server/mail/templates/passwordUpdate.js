0cc9efaedb30209e845f403ae14c23fa1a848778
Kapil Paliwal <paliwalkapil2004@gmail.com>
2024-08-11 00:03:37 +0530

    chore: Update LearnSphere logo URL in passwordUpdate.js

--- server/mail/templates/passwordUpdate.js
+++ server/mail/templates/passwordUpdate.js
@@ -55,8 +55,8 @@ exports.passwordUpdated = (email, name) => {
     
     <body>
         <div class="container">
-            <a href="https://study-notion-kapil-2305.vercel.app"><img class="logo"
-                    src="https://res.cloudinary.com/dgpkeaffc/image/upload/v1723297508/logo-black_d64qsn.png" alt="LearnSphere Logo"></a>
+            <a href="https://learn-sphere-kapil-2305.vercel.app/"><img class="logo"
+                    src="https://res.cloudinary.com/dgpkeaffc/image/upload/v1723297508/logo-no-background_qlul06.png" alt="LearnSphere Logo"></a>
             <div class="message">Password Update Confirmation</div>
             <div class="body">
                 <p>Hey ${name},</p>
